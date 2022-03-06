c7274700c8b71386b4050de0defc6029a954feba
Marcelo Cardoso <marcelosouza7r@gmail.com>
2022-03-05 23:04:16 -0300

    Algoritmo de Bissecção

---
+++ bisseccao.py
@@ -0,0 +1,23 @@
+
+def f(x):
+    return (x**2) - 7
+
+
+def bisseccao(xa, xb, precisao):
+    xm = 99999
+    if f(xa)*f(xb) < 0:
+        while (abs(f(xm)) > precisao):
+            xm = (xa + xb) / 2
+            if f(xm) == 0:
+                print(f'Raiz = {xm:.2f}')
+            else:
+                if f(xa)*f(xm) < 0:
+                    xb = xm
+                else:
+                    xa = xm
+        print(f'Raiz = {xm:.2f}')
+    else:
+        return print("Insira novos valores para Xa e Xb")
+
+
+bisseccao(-3, 2, 0.01)
